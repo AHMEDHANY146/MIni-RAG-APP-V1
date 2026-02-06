fae4c3f5b28ae11245a80e0801a5f59b2c35ad4f
AHMEDHANY146 <ah1481200@gmail.com>
2026-02-06 16:47:37 +0200

    Update data.py

--- src/routes/schemas/data.py
+++ src/routes/schemas/data.py
@@ -5,7 +5,7 @@ class ProcessRequest(BaseModel):
     file_id: str = None
     chunk_size: Optional[int] = 1024
     overlap_size: Optional[int] = 20
-    do_reset : Optional[int] = 0
+    do_reset : Optional[int] = 1
 
 
 
